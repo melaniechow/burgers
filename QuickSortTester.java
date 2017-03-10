bb95c9062dd1207e0c6f92ca9119821120d139f6
echen8 <echen8@stuy.edu>
2017-03-09 22:42:41 -0500

    Updated QuickSortTester.java, created .gitignore

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -1,38 +1,40 @@
 /*
-Burgers - Eric Chen, Aryan Bhatt, Melanie Chow
+Burgers -- Aryan Bhatt, Melanie Chow, Eric Chen
 APCS2 pd4
-Lab01 -- What Does the Data Say?
-2017-03-10
+LAB01 -- What Does the Data Say?
+2017-03-08
  */
 
-/*======================================
-  class MergeSortTester
+import cs1.Keyboard;
+
+/**======================================
+  class QuickSortTester
 
   ALGORITHM:
-  First, it checks if the length of the given array 'arr' is 1, and if so, it return arr.  Otherwise, it splits arr into two different int arrays: a and b.  It then merges the two after they get recursively sorted.
 
   BIG-OH CLASSIFICATION OF ALGORITHM:
   O(n)
 
   Mean execution times for dataset of size n:
   Batch size: 10000
-  n=1       time: 64
-  n=10      time: 875
-  n=100     time: 11524
-  n=200     time: 22394
-  n=300     time: 34510
-  n=400     time: 46232
-  n=500     time: 58646
+  n=1       time: <time>
+  n=10      time: <time>
+  n=100     time: <time>
+  n=200     time: <time>
+  n=300     time: <time>
+  n=400     time: <time>
+  n=500     time: <time>
   ...
-  n=10000   time: 1462788
+  n=10000   time: <time>
 
   ANALYSIS:
-  The algorithm is O(n) runtime, as shown by a graph of time versus n, which is more or less a straight line.
-  ======================================*/
+  
 
-public class QuickSortTester 
-{
-    /*quicksort code*/
+  ======================================**/
+
+public class QuickSortTester {
+    
+    // Melanie's QuickSort.java
     public static void swap(int[] arr, int i, int j){
 	int first=arr[i];
 	int second=arr[j];
@@ -67,89 +69,53 @@ public class QuickSortTester
 	    int pvtPos=partition(arr,left,right,(left+right)/2);
 	    quicksort(arr,left, pvtPos-1);
 	    quicksort(arr,pvtPos+1,right);
-	    }
+	}
     }
 
     /******************************
      * execution time analysis 
      * We find the start time in nanoseconds, run the sort() method, then subtract it from the end time in nanoseconds.  This number is added to a total, which will be divided by the batch size and printed to the terminal.
      ******************************/
-    public static void main( String[] args ) 
-    {
+    public static void main( String[] args ) {
 
-	// SET BATCHSIZE HERE
-	int batchSize = 10000;
+	System.out.print( "Please specify test array size: " );
+	int size = Keyboard.readInt();
+	System.out.print( "Please set how many trials you would like to run: " );
+	int batchSize = Keyboard.readInt();
 
-	// SET BIGSIZE HERE
-	int bigSize = 500;
+	/**
+	 // SET BATCHSIZE HERE
+	 int batchSize = 10000;
+
+	 // SET SIZE HERE
+	 int size = 500;
+	**/
 	
+	int[] n = new int[size];
 	
-	int[] n1 = new int[1];
-	int[] n10 = new int[10];
-	int[] n100 = new int [100];
-	int[] nbs = new int[bigSize];
+	long t = 0;
 
-	long n1t = 0;
-	long n10t = 0;
-	long n100t = 0;
-	long nbst = 0;
-
-	// Runs 10000 times
+	// Runs batchSize times
 	for ( int j = 0 ; j < batchSize ; j++ ) {
-
-	    n1[0] = (int)( Math.random() * 5 );
-
-	    for ( int i = 0 ; i < 10 ; i++ ) {
-		n10[i] = (int)( Math.random() * 50 );
-	    }
-
-	    for ( int i = 0 ; i < 100 ; i++ ) {
-		n100[i] = (int)( Math.random() * 500 );
-	    }
-
-	    for ( int i = 0 ; i < bigSize ; i++ ) {
-		nbs[i] = (int)( Math.random() * 1000 );
+	    for ( int i = 0 ; i < size ; i++ ) {
+		n[i] = (int)( Math.random() * 1000 );
 	    }
 
 	    // Runtime Finder
 	    long start;
 	    long elapsed;
 
-	    // n1
-	    start = System.nanoTime();
-	    quicksort( n1,0,n1.length-1 );
-	    elapsed = System.nanoTime() - start;
-	    n1t += elapsed;
-
-	    // n10
-	    start = System.nanoTime();
-	    quicksort( n10,0,n10.length-1 );
-	    elapsed = System.nanoTime() - start;
-	    n10t += elapsed;
-
-	    // n100
-	    start = System.nanoTime();
-	    quicksort( n100,0,n100.length-1 );
-	    elapsed = System.nanoTime() - start;
-	    n100t += elapsed;
-
 	    // n of bigSize
 	    start = System.nanoTime();
-	    quicksort( nbs,0,nbs.length-1 );
+	    quicksort( n, 0, size - 1 );
 	    elapsed = System.nanoTime() - start;
-	    nbst += elapsed;
+	    t += elapsed;
 	    
 	}
 
-	n1t /= batchSize;
-	n10t /= batchSize;
-	n100t /= batchSize;
-	nbst /= batchSize;
-
-	System.out.println( "    Average runtime with 1 item: " + n1t );
-	System.out.println( "  Average runtime with 10 items: " + n10t );
-	System.out.println( " Average runtime with 100 items: " + n100t );
-	System.out.println( " Average runtime with " + bigSize + " items: " + nbst );
+	t /= batchSize;
+	
+	System.out.println( " Average runtime for " + batchSize + " runs with " + size + " items: " + t );
 
     }//end main
 
